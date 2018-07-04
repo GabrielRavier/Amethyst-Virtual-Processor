7774fb5b0e7e3776f7e10da8a8789b34ed2f951a
CxpcakeTheDerp <he.beackerdan@gmail.com>
2018-07-04 04:44:06 -0600

    Update Stack.c

--- bytecode_execute/Stack.c
+++ bytecode_execute/Stack.c
@@ -1,21 +1,42 @@
+// Programmed with luv. ~~kernels ♥♥♥
+
 #include "interpreter.h"
+#include "Variables.c"
 #include <stdio.h>
 
-void PushStackAddressData()
+// Push Address and Data to stack
+void PushStackAddressData(int address, int data)
 {
+	StackAddress[TopLevelIndex + 1] = address;
+	StackData[TopLevelIndex + 1] = data;
+	TopLevelIndex++;
 
+	lv_UpdateStackRegisters(); // Just updates the variables
 }
 
-void PushStackData()
+// Push Data to stack
+void PushStackData(int data)
 {
+	StackAddress[TopLevelIndex + 1] = prc;
+	StackData[TopLevelIndex + 1] = data;
+	TopLevelIndex++;
 
+	lv_UpdateStackRegisters();
 }
 
+// Pop Stack once
 void PopStack()
 {
+	StackAddress[TopLevelIndex] = 0;
+	StackData[TopLevelIndex] = 0;
+	TopLevelIndex--;
 
+	lv_UpdateStackRegisters();
 }
+
+// LOWLEVEL: Update registers
 void lv_UpdateStackRegisters()
 {
-
+	sta = StackAddress[TopLevelIndex];
+	std = StackAddress[TopLevelIndex];
 }
