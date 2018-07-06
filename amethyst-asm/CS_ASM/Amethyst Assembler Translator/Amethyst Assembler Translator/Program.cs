3fd8ba7e1214603c3cdde9f791ffcff8bacb89be
CxpcakeTheDerp <he.beackerdan@gmail.com>
2018-07-06 01:14:56 -0600

    yay C# assembly translator woot

---
+++ amethyst-asm/CS_ASM/Amethyst Assembler Translator/Amethyst Assembler Translator/Program.cs
@@ -0,0 +1,31 @@
+﻿// Programmed with luv. ~~kernels ♥♥♥
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amethyst_Assembler_Translator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
