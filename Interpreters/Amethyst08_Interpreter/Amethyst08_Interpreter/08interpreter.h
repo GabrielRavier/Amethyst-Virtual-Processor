849d586e0f3adb404bc936f30c3dc4d444a6d0ab
CxpcakeTheDerp <he.beackerdan@gmail.com>
2018-08-02 03:39:04 -0600

    Started work on TODO list.

--- Interpreters/Amethyst08_Interpreter/Amethyst08_Interpreter/08interpreter.h
+++ Interpreters/Amethyst08_Interpreter/Amethyst08_Interpreter/08interpreter.h
@@ -1,1 +1,41 @@
-#pragma once
+﻿#pragma once
+// Programmed with luv.. ~~kernels ♥♥♥
+
+// [ TASK LIST -- Amethyst 0.8 Interpreter ]
+// =========================================
+//
+// TODO: Use Define Preprocesors to ease torture.
+// TODO: Add 1 instr_size instructions
+// TODO: Add 2 instr_size instructions
+// TODO: Add 3 instr_size instructions
+// TODO: Add 4 instr_size instructions
+// TODO: Comment
+// 
+// Unit Test
+// =========
+// TODO: UNIT TESTS!!!
+// TODO: Overall function
+// TODO: instr_size 1 and 2
+// TODO: instr_size 3 and 4
+//
+// Systems Test
+// ============
+// TODO: Full Systems Test
+// TODO: Write sample code for Systems Test
+// TODO: DevOps cycle and clean code.
+//
+// Maintenance
+// ===========
+// TODO: WILL DO - MORE AND MORE COMMENTS.
+// TODO: Save kernel's brain memory state.
+//
+// Mentor Help and Rating Documentations
+// =====================================
+//
+//
+//
+// General TODO List
+// =================
+// TODO: Write TODO list before taking a 8 hour nap
+// TODO: Don't forget to write a self-deprecating message to your future self reading this code.
+// TODO: Stove warm some milk and sugar.
